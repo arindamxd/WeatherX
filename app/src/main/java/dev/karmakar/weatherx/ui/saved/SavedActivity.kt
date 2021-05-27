a91e35f859d334630798d611a6e915a05f51ec0a
Arindam Karmakar <arindamxd@gmail.com>
2021-05-28 03:05:14 +0530

    cleanups

--- app/src/main/java/dev/karmakar/weatherx/ui/saved/SavedActivity.kt
+++ app/src/main/java/dev/karmakar/weatherx/ui/saved/SavedActivity.kt
@@ -1,7 +1,6 @@
 package dev.karmakar.weatherx.ui.saved
 
 import android.os.Bundle
-import android.view.View
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import dev.karmakar.weatherx.R
@@ -13,7 +12,6 @@ import dev.karmakar.weatherx.ui.shared.SharedAdapter
 import dev.karmakar.weatherx.ui.shared.SharedViewModel
 import dev.karmakar.weatherx.util.ViewModelFactory
 import kotlinx.android.synthetic.main.activity_saved.*
-import kotlinx.android.synthetic.main.activity_search.progress
 import kotlinx.android.synthetic.main.activity_search.recycler_view
 
 /**
@@ -32,13 +30,17 @@ class SavedActivity : AppCompatActivity() {
 
         viewModel = initViewModel()
         viewModel.observeSaved().observe(this, { weatherList ->
-            toggleProgress(false)
             recycler_view.adapter = SharedAdapter(weatherList) { weatherId ->
                 viewModel.toggleSaved(weatherId)
             }
         })
 
         back.setOnClickListener { onBackPressed() }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        viewModel.observeSaved().removeObservers(this)
     }
 
     // endregion Lifecycle
@@ -51,15 +53,5 @@ class SavedActivity : AppCompatActivity() {
             ))
         }
         return model
-    }
-
-    private fun toggleProgress(enable: Boolean) {
-        if (enable) {
-            progress.visibility = View.VISIBLE
-            recycler_view.visibility = View.GONE
-        } else {
-            progress.visibility = View.GONE
-            recycler_view.visibility = View.VISIBLE
-        }
     }
 }
